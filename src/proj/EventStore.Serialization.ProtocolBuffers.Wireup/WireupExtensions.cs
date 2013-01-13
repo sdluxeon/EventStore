2bf7dee95b6302aa872ac4d626c21ee52d85d248
Nikolay Mynkow <nikolai.mynkow@blogtronix.com>
2013-01-13 19:13:39 +0200

    Move the protocol buffers serialization from the protobuf branch to master. The current version is naive but it is working for me. Kudos to sd.luxeon

---
+++ src/proj/EventStore.Serialization.ProtocolBuffers.Wireup/WireupExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace EventStore
+{
+    using Serialization;
+    using System.Reflection;
+    using System;
+
+    public static class WireupExtensions
+    {
+        public static SerializationWireup UsingProtocolBuffersSerialization(this PersistenceWireup wireup)
+        {
+            return wireup.UsingCustomSerialization(new ProtocolBufferSerializer());
+        }
+
+        public static SerializationWireup UsingProtocolBuffersSerialization(this PersistenceWireup wireup, params string[] contractAssemblyFileNamePatterns)
+        {
+            return wireup.UsingCustomSerialization(new ProtocolBufferSerializer(contractAssemblyFileNamePatterns));
+        }
+
+        public static SerializationWireup UsingProtocolBuffersSerialization(this PersistenceWireup wireup, params Assembly[] contractAssemblies)
+        {
+            return wireup.UsingCustomSerialization(new ProtocolBufferSerializer(contractAssemblies));
+        }
+
+        public static SerializationWireup UsingProtocolBuffersSerialization(this PersistenceWireup wireup, params Type[] dataContracts)
+        {
+            return wireup.UsingCustomSerialization(new ProtocolBufferSerializer(dataContracts));
+        }
+    }
+}
